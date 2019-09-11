311a180cba942816f75416ab3ad75e0783e454f9
Zinkevich Pavel <pavel.zinkevich@innowise-group.com>
2019-09-11 19:43:56 +0300

    implement readAll method

--- src/main/java/by/siegell/soa/clinic/dao/impl/DoctorDaoImpl.java
+++ src/main/java/by/siegell/soa/clinic/dao/impl/DoctorDaoImpl.java
@@ -1,8 +1,14 @@
 package by.siegell.soa.clinic.dao.impl;
 
 import by.siegell.soa.clinic.dao.DoctorDao;
+import by.siegell.soa.clinic.db.ConnectionFactory;
 import by.siegell.soa.clinic.domain.Doctor;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DoctorDaoImpl implements DoctorDao {
@@ -28,6 +34,46 @@ public class DoctorDaoImpl implements DoctorDao {
 
     @Override
     public List<Doctor> readAll() {
-        return null;
+        String sql = "select `id`, `firstName`, `middleName`, `lastName`, `specialization`,  `district`, `cabinet`, `createdAt`, `updatedAt`"
+                + "from `doctor`";
+        Connection c = null;
+        Statement s = null;
+        ResultSet r = null;
+        LinkedList<Doctor> doctors = new LinkedList<>();
+        try {
+            c = ConnectionFactory.getConnection();
+            s = c.createStatement();
+            r = s.executeQuery(sql);
+            while (r.next()) {
+                Doctor doctor = Doctor.builder()
+                        .firstName(r.getString("firstName"))
+                        .middleName(r.getString("middleName"))
+                        .lastName(r.getString("lastName"))
+                        .specialization(r.getString("specialization"))
+                        .district(r.getString("district"))
+                        .cabinet(r.getString("cabinet"))
+                        .build();
+                doctor.setId(r.getLong("id"));
+                doctor.setCreatedAt(r.getTimestamp("createdAt"));
+                doctor.setUpdatedAt(r.getTimestamp("updatedAt"));
+                doctors.add(doctor);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                r.close();
+            } catch (NullPointerException | SQLException e) {
+            }
+            try {
+                s.close();
+            } catch (NullPointerException | SQLException e) {
+            }
+            try {
+                c.close();
+            } catch (NullPointerException | SQLException e) {
+            }
+        }
+        return doctors;
     }
 }
