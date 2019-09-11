1fa2bf2720b560e7b77ae9d17f7fa5541994449f
Zinkevich Pavel <pavel.zinkevich@innowise-group.com>
2019-09-11 19:16:55 +0300

    implement jdbc connection factory

---
+++ src/main/java/by/siegell/soa/clinic/constants/DbConstants.java
@@ -0,0 +1,8 @@
+package by.siegell.soa.clinic.constants;
+
+public class DbConstants {
+    public static final String URL = "";
+    public static final String USERNAME = "clinic";
+    public static final String PASSWORD = "clinic";
+    public static final String JDBC_DRIVER = "org.mariadb.jdbc.Driver";
+}
---
+++ src/main/java/by/siegell/soa/clinic/db/ConnectionFactory.java
@@ -0,0 +1,30 @@
+package by.siegell.soa.clinic.db;
+
+import by.siegell.soa.clinic.constants.DbConstants;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static String jdbcUrl;
+    private static String jdbcUser;
+    private static String jdbcPassword;
+
+    static {
+        try {
+            Class.forName(DbConstants.JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        jdbcUrl = DbConstants.URL;
+        jdbcUser = DbConstants.USERNAME;
+        jdbcPassword = DbConstants.PASSWORD;
+    }
+
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(jdbcUrl,
+                jdbcUser,
+                jdbcPassword);
+    }
+}
